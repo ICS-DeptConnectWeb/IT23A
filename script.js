5dff213636effeb4bbf402596e25af6eef63ed9b
ElizhaSegura <elizhaalthea@gmail.com>
2025-10-22 22:21:17 +0800

    Added credits section and links

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("knowMeBtn");
+
+  button.addEventListener("click", function () {
+    alert("You clicked it. There's no turning back now.");
+  });
+});
