a568489f285bd9c03b38bb20170c9f53d6a7f805
ElizhaSegura <elizhaalthea@gmail.com>
2025-11-26 23:59:08 +0800

    Faculty section not finalized

--- Elective 1/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.getElementById("knowMeBtn");
-
-  button.addEventListener("click", function () {
-    alert("You clicked it. There's no turning back now.");
-  });
-});
