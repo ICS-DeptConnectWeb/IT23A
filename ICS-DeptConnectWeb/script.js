caea93e2864bf105992569f6ee3b888ef07a8e4f
ElizhaSegura <elizhaalthea@gmail.com>
2025-12-13 22:18:39 +0800

    Swap logo position on footer

--- ICS-DeptConnectWeb/script.js
+++ ICS-DeptConnectWeb/script.js
@@ -1,2 +1,18 @@
 
 
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.getElementById('navbar');
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+    hamburger.onclick = () => {
+        navbar.classList.toggle('active');
+    };
+
+    dropdowns.forEach(drop => {
+        drop.querySelector('.dropbtn').onclick = function(e) {
+            if (window.innerWidth <= 900) {
+                e.preventDefault();
+                drop.classList.toggle('open');
+            }
+        };
+    });
